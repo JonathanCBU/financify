3dd68ee290c6d5275bb2b7c5ed0f933c18ae8686
Jonathan Cook <jcookbme@gmail.com>
2024-02-15 23:12:43 -0500

    Major clean up

--- financify/library/excel_manager.py
+++ financify/library/excel_manager.py
@@ -2,13 +2,40 @@
 
 import os
 from datetime import datetime
-from operator import itemgetter
+from operator import attrgetter
 from typing import Any, Dict, List
 
 import pylightxl
 
+from financify.library.exceptions import NoPropException
+
 Row = List[Any]
 Sheet = List[Row]
+
+
+class AssetEvent:
+    """Property indexible event for Assets"""
+
+    def __init__(self, **kwargs: str) -> None:
+        """Parse key word args"""
+        self.__dict__.update(
+            (key, kwargs[key])
+            for key in ("date", "id", "type", "value")
+            if key in kwargs
+        )
+
+    def timestamp(self, date_pattern: str = "%Y/%M/%d") -> datetime:
+        """Return date as a datetime
+
+        :param date_pattern: optional regex pattern
+        """
+        if hasattr(self, "date"):
+            return datetime.strptime(self.date, date_pattern)
+        raise NoPropException("Asset Event does not have a date property")
+
+    def update(self, **kwargs: str) -> None:
+        """Update props with extra data"""
+        self.__dict__.update(kwargs)
 
 
 class ExcelReader:
@@ -23,77 +50,28 @@ class ExcelReader:
         self.cfg = cfg
         self.database = pylightxl.readxl(os.path.join(read_loc, self.cfg["file_name"]))
         self.sheet = self.database.ws(self.cfg["sheets"][0])
-        self.columns: Sheet = list(self.sheet.cols)
-        self.rows: Sheet = list(self.sheet.rows)
+        self.rows: List[AssetEvent] = []
+        self.headers = [header.strip() for header in self.sheet.row(1)]
+        self.row_props = list(self.cfg["columns"].keys())
+        flagged_cols: Dict[str, int] = {}
+        for col, header in self.cfg["columns"].items():
+            flagged_cols[col] = self.headers.index(header) + 1
+        for row in list(self.sheet.rows)[1:]:
+            asset_evt = dict(
+                map(
+                    lambda key, val: (key, val),
+                    flagged_cols.keys(),
+                    [row[idx - 1] for idx in flagged_cols.values()],
+                )
+            )
+            self.rows.append(AssetEvent(**asset_evt))
 
-    @property
-    def wash_sale_cols(self) -> Dict[str, int]:
-        """Get relevant wash sale column numbers"""
-        headers = [header.strip() for header in self.sheet.row(1)]
-        return {
-            "date": headers.index(self.cfg["columns"]["date"]) + 1,
-            "transaction": headers.index(self.cfg["columns"]["transaction"]) + 1,
-            "id": headers.index(self.cfg["columns"]["id"]) + 1,
-            "price": headers.index(self.cfg["columns"]["price"]) + 1,
-        }
+    def sort_by(self, props: List[str]) -> None:
+        """Sort rows list by props in place
 
-    @property
-    def idx_map(self) -> Dict[str, int]:
-        """pruned sheet indexes map"""
-        return {
-            "id": list(self.wash_sale_cols.keys()).index("id"),
-            "transaction": list(self.wash_sale_cols.keys()).index("transaction"),
-            "date": list(self.wash_sale_cols.keys()).index("date"),
-            "price": list(self.wash_sale_cols.keys()).index("price"),
-        }
-
-    def prune_wash_sale_rows(self) -> Sheet:
-        """Prune irrelevant columns from data"""
-        relevant_cols = [
-            self.wash_sale_cols["date"],
-            self.wash_sale_cols["transaction"],
-            self.wash_sale_cols["id"],
-            self.wash_sale_cols["price"],
-        ]
-        pruned_rows = []
-        for row in self.rows:
-            pruned_rows.append([row[idx - 1] for idx in relevant_cols])
-        return pruned_rows
-
-    def sort_by_id(self, rows: Sheet, date_pattern: str = "%Y/%M/%d") -> Sheet:
-        """Sort sheet by id and dates
-
-        ..Note: https://docs.python.org/3/library/datetime.html#strftime-strptime-behavior
-
-        :param rows: a sheet object to be sorted
-        :param date_pattern: datetime from string format following table in note
+        :param props: List of properties to sort rows by
         """
-        for row in rows[1:]:
-            row[0] = datetime.strptime(row[0], date_pattern)
-        return sorted(
-            rows[1:],
-            key=itemgetter(
-                self.idx_map["id"],
-                self.idx_map["date"],
-            ),
-        )
+        self.rows.sort(key=attrgetter(*props))
 
-    def get_id_sheets(self, rows: Sheet) -> List[Sheet]:
-        """Return a list of Sheet objects for each unique asset id
-
-        :param rows: sheet sorted by ID (and ideally secondarily sorted by date)
-        """
-        sheets: List[Sheet] = []
-        current_id = ""
-        current_sheet: Sheet = []
-        for row in rows:
-            if row[self.idx_map["id"]] != current_id:
-                # new sheet
-                if current_sheet:
-                    sheets.append(current_sheet)
-                current_id = row[self.idx_map["id"]]
-                current_sheet = [row]
-            else:
-                current_sheet.append(row)
-        sheets.append(current_sheet)
-        return sheets
+    def flag_losses(self) -> None:
+        """Update loss sale rows"""
--- financify/library/exceptions.py
+++ financify/library/exceptions.py
@@ -9,5 +9,13 @@ class PlatformException(Exception):
     """Parent Class for Platform Exceptions"""
 
 
+class MemberException(Exception):
+    """Parent class for class member exceptions"""
+
+
 class InvalidOSException(PlatformException):
     """Raised when operating on an unsupported OS"""
+
+
+class NoPropException(MemberException):
+    """Raised when referencing a non-existent class prop"""
